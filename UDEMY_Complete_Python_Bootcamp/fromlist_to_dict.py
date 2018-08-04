a2444d6810c36f7075081f20f6975ec9e55ff4fb
ronek88 <rtischler11@gmail.com>
2018-08-04 12:12:21 +0200

    Examples for reconverting from list to dict..

--- UDEMY_Complete_Python_Bootcamp/fromlist_to_dict.py
+++ UDEMY_Complete_Python_Bootcamp/fromlist_to_dict.py
@@ -3,13 +3,17 @@ l2 = [1,1,1,1,1]
 s = dict(zip(l1,l2))
 print(s)
 
+input('After command is finished press Enter!')
+
 l1_after = [12541,12546,21659,16448,15488]
 l2_after = [0,0,0,0,0]
 s2       = dict(zip(l1_after,l2_after))
 print(s2)
 
-keys_s = s.keys()
+keys_s  = s.keys()
 print(keys_s)
+keys_s2 = s2.keys()
+print(keys_s2)
 
 val_s  = s.values()
 val_s2 = s2.values()
@@ -22,9 +26,8 @@ print(s[12541])
 len_d = len(s)
 for i in keys_s:
     if s[i] == 0 and s2[i] == 1:
-        print('All is OK for S1' + str(i) +'ID: ' + str(s[i]) + ' value, and for S2' + str(i) + 'ID: ' + str(s2[i]))
+        print('All is OK for D1 ' + str(i) +' ID: ' + str(s[i]) + ' value, and for D2: ' + str(i) + ' ID: ' + str(s2[i]))
     elif s[i] == 1 and s2[i] == 0:
-        print('All is OK for S1' + str(i) + 'ID: ' + str(s[i]) + ' value, and for S2' + str(i) + 'ID: ' + str(s2[i]))
+        print('All is OK for D1 ' + str(i) + ' ID: ' + str(s[i]) + ' value, and for D2: ' + str(i) + ' ID: ' + str(s2[i]))
     else:
         assert False
-
